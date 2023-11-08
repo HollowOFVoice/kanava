f736c8ad6809502986c0dc689d8c9d2e2a647ca4
HollowOFVoice <assasinstwo5@gmail.com>
2023-11-08 15:45:45 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,13 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
                 System.out.println("i = " + i);
+
+
+
+
+            System.out.print("Штирлиц безуспешно пытался установить личность. Личность всё время падала.");
+            System.out.print("Курица клевала носом.\n" +
+                    "\"Наверное не выспалась\", - подумал Штирлиц");
         }
     }
 }
